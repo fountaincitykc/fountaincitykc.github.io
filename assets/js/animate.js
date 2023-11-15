87c7c23d360cf6dbe9b95e40e1c8fcaa987bcaef
Travis Beckham <howdytravis@gmail.com>
2023-11-15 15:07:15 -0600

    Reorganized assets

---
+++ assets/js/animate.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', function(event) {
+
+  // Animate all elements with the .animate classname
+  // (see .animate and .js-start-animation in main.css)
+
+  const animationTargets = document.querySelectorAll('.animate');
+  if (animationTargets) {
+    animationTargets.forEach(t => {
+      let observer = new IntersectionObserver(e => {
+        e.forEach(e => {
+          if (e.isIntersecting) {
+            e.target.classList.add('js-start-animation');
+            observer.unobserve(e.target);
+          }
+        });
+      });
+      observer.observe(t);
+    });
+  }
+}, false);
