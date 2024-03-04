4d9899743d1342ea02a81e9e4c633c105a5bc62c
Travis Beckham <howdytravis@gmail.com>
2024-03-04 14:16:57 -0600

    Overhauled css class names

--- assets/js/main-nav.js
+++ assets/js/main-nav.js
@@ -1,15 +1,15 @@
 window.addEventListener('DOMContentLoaded', function(event) {
 
   // Create mobile nav menu
-  // (see .js-nav, .js-nav-toggle, and .js-nav-close in components/_main-nav.css)
+  // (see .js-nav, .js-main-nav--toggle, and .js-main-nav--close in components/_main-nav.css)
 
   const mainNav = document.getElementById('main-nav');
   if (mainNav) {
     // Add classname to body
     document.body.classList.add('js-nav');// Hides .main-nav by default
-    // Create .js-nav-toggle to toggle the display of nav
+    // Create .js-main-nav--toggle to toggle the display of nav
     const navToggle = document.createElement('div');
-    navToggle.className = 'js-nav-toggle';
+    navToggle.className = 'js-main-nav--toggle';
     const navToggleLink = document.createElement('span');
     navToggleLink.appendChild(document.createTextNode('Menu'));
     navToggleLink.addEventListener('click', function(e){
@@ -17,11 +17,11 @@ window.addEventListener('DOMContentLoaded', function(event) {
       document.body.classList.toggle('show-nav');
     });
     navToggle.appendChild(navToggleLink);
-    // Insert .js-nav-toggle before .main-nav
+    // Insert .js-main-nav--toggle before .main-nav
     mainNav.parentNode.insertBefore(navToggle, mainNav);
-    // Create .js-nav-close to close nav
+    // Create .js-main-nav--close to close nav
     const navClose = document.createElement('div');
-    navClose.className = 'js-nav-close';
+    navClose.className = 'js-main-nav--close';
     const navCloseLink = document.createElement('span');
     navCloseLink.appendChild(document.createTextNode('Close'));
     navCloseLink.addEventListener('click', function(e){
@@ -29,16 +29,16 @@ window.addEventListener('DOMContentLoaded', function(event) {
       document.body.classList.remove('show-nav');
     });
     navClose.appendChild(navCloseLink);
-    // Insert .js-nav-close as the first child of .main-nav
+    // Insert .js-main-nav--close as the first child of .main-nav
     mainNav.prepend(navClose);
   }
   /*
   Result:
   <body class="js-nav">
     ...
-    <div class="js-nav-toggle"><span>Menu</span></div>
+    <div class="js-main-nav--toggle"><span>Menu</span></div>
     <nav id="main-nav" class="main-nav">
-      <div class="js-nav-close"><span>Close</span></div>
+      <div class="js-main-nav--close"><span>Close</span></div>
       <ul>...
   */
 }, false);
