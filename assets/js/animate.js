635246398bde376366261117768ae74fc3438015
Travis Beckham <howdytravis@gmail.com>
2026-08-03 11:41:57 -0500

    Improve the fade in transition so it's only applied if Javascript is enabled

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,15 +1,17 @@
-window.addEventListener("DOMContentLoaded", function(event) {
+window.addEventListener("DOMContentLoaded", function (event) {
 
-  // Animate all elements with the .animate classname
-  // (see .animate and .js-start-animation in components/_animate.css)
+  // Animate all elements with the .animate classname when they become visible in the viewport
+  // See: assets/scss/components/_animate.scss
 
-  const animationTargets = document.querySelectorAll(".animate");
-  if (animationTargets) {
-    animationTargets.forEach(t => {
+  const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+  const targets = document.querySelectorAll(".animate");
+  if (!prefersReducedMotion && targets) {
+    targets.forEach(t => {
+      t.classList.add("animate--start");
       let observer = new IntersectionObserver(e => {
         e.forEach(e => {
           if (e.isIntersecting) {
-            e.target.classList.add("js-start-animation");
+            e.target.classList.add("animate--end");
             observer.unobserve(e.target);
           }
         });
